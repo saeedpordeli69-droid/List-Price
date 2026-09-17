b7ee4d33c2cf76eac16cc8ac1b4ab48f1ee03d90
saeedpordeli69-droid <saeedpordeli69@gmail.com>
2026-09-18 02:45:17 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,21 @@
-const CACHE="price-manager-v2";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="price-manager-v3";
+
+self.addEventListener("install",e=>
+  e.waitUntil(
+    caches.open(CACHE).then(c=>
+      c.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ])
+    )
+  )
+);
+
+self.addEventListener("fetch",e=>
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  )
+);
